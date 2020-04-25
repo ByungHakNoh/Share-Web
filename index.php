27f7c6a4176c75d82b23e4c2f6178755193ec024
ByungHakNoh <nbh6579@naver.com>
2020-04-25 09:53:46 +0900

    "[add] 커스텀 라우터를 사용하기 위한 index파일 생성"

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-require 'view/view_index.php';
+require 'core/CoreBundle.php';
+
+$uri = RequestURI::uriPath();
+// $uri = trim($_SERVER['REQUEST_URI'], '/');
+// var_dump($app);
+
+require Router::load('routes.php')->direct($uri);
