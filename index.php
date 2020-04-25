49fd4b3368ddc4fd5f85ce60a4d1e39ec1dee339
ByungHakNoh <nbh6579@naver.com>
2020-04-25 09:54:52 +0900

    "[sort] Request uri를 담당하는 클래스 구현 후 주석 지움"

--- index.php
+++ index.php
@@ -3,7 +3,5 @@
 require 'core/CoreBundle.php';
 
 $uri = RequestURI::uriPath();
-// $uri = trim($_SERVER['REQUEST_URI'], '/');
-// var_dump($app);
 
 require Router::load('routes.php')->direct($uri);
