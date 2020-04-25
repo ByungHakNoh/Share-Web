27f744736d8a11acb5943769e470ee00aea1bc22
ByungHakNoh <nbh6579@naver.com>
2020-04-25 09:55:51 +0900

    "[add] key-value 값으로 저장한 라우트 구현"

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+$router->define([
+    '' => 'controllers/index.php',
+    'news' => 'controllers/news.php',
+    'information' => 'controllers/information.php',
+    'board' => 'controllers/board.php',
+    'broadcast' => 'controllers/broadcast.php',
+    'login' => 'controllers/login.php',
+    'register' => 'controllers/register.php',
+    'practice' => 'controllers/1_practice.php',
+]);
