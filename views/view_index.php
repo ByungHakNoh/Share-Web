afabfdd7335033fcb5389777b812916fa2628343
ByungHakNoh <nbh6579@naver.com>
2020-04-26 17:13:57 +0900

    "[modify] div 클래스명 변경"

--- views/view_index.php
+++ views/view_index.php
@@ -2,24 +2,28 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Share</title>
-    <link rel="stylesheet" href="css/style.css" />
+    <?php require('holder/main/linkCss.php'); ?>
 </head>
 
 <body>
 
-    <?php require('holder/mainHeadPage.php'); ?>
+    <?php require('holder/main/bodyHeadPage.php'); ?>
 
-    <div class="post_box">
-        패션 소식
-    </div>
+    <div class="homeMainBoard">
+        <div class="M">
+            패션 소식
+        </div>
 
-    <div class="post_box">
-        패션 정보
-    </div>
+        <div class="M">
+            패션 정보
+        </div>
 
-    <div class="post_box">
-        자유 계시판
+        <div class="M">
+            자유 계시판
+        </div>
     </div>
 </body>
 
