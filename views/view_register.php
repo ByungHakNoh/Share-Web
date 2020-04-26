99773115f92e6c70dd42b31d3530c0fbd05a28c3
ByungHakNoh <nbh6579@naver.com>
2020-04-26 17:14:51 +0900

    "[add] 회원가입 레이아웃 추가"

--- views/view_register.php
+++ views/view_register.php
@@ -2,13 +2,40 @@
 <html lang="en">
 
 <head>
-    <title>회원가입</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>회원가입</title>
+    <?php require('holder/signIn/linkCss.php'); ?>
 </head>
 
 <body>
-    <h1>회원가입</h1>
+
+    <div class=container>
+        <img src="src/image/register.png" class="icon">
+        <h1>회원가입 하기</h1>
+
+        <div class="warnging">
+            <ul>
+                <?php foreach ($errorsArray as $error) : ?>
+                <li><?= $error ?></li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+
+        <form method="POST" action="/register">
+            <p>사용자 아이디</p>
+            <input type="text" name="userID" placeholder="이메일 입력" value=<?= $userID ?>>
+
+            <p>비밀번호</p>
+            <input type="password" name="password" placeholder="비밀번호 입력" value=<?= $password ?>>
+
+            <p>비밀번호 확인</p>
+            <input type="password" name="passwordCheck" placeholder="비밀번호 확인" value=<?= $passwordCheck ?>>
+
+            <input type="submit" value="회원가입">
+        </form>
+    </div>
+
 </body>
 
 </html>
