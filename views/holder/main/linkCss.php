151af740ca9a91bcba0375969d288776a2ecfc70
ByungHakNoh <nbh6579@naver.com>
2020-04-26 17:13:21 +0900

    "[add] css 파일 링크 루트를 따로 관리"

---
+++ views/holder/main/linkCss.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="css/mainStyle.css?after" />
--- views/view_information.php
+++ views/view_information.php
@@ -2,12 +2,16 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Share - information</title>
-    <link rel="stylesheet" href="../css/style.css" />
+    <?php require('holder/main/linkCss.php'); ?>
 </head>
 
 <body>
-    <?php require('holder/mainHeadPage.php'); ?>
+
+    <?php require('holder/main/bodyHeadPage.php'); ?>
+
 </body>
 
 </html>
