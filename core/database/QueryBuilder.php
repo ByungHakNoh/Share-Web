85ce709c448ecc40b4016e5ca676cfe6d706ee14
ByungHakNoh <nbh6579@naver.com>
2020-04-26 17:17:56 +0900

    "[add] 데이터베이스에 데이터 저장하는 메소드 추가"

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -12,6 +12,7 @@ class QueryBuilder
 
     public function selectTable(String $table)
     {
+        // Memo : prepare은 PDO 클래스 메소드이다.
         $statement = $this->pdo->prepare("select * from {$table}");
         $statement->execute();
         return $statement;
@@ -20,5 +21,26 @@ class QueryBuilder
     public function fetchAllValues($statement)
     {
         return $statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function insertData(String $table, $keyValueData)
+    {
+        $stringFormat = sprintf(
+            'insert into %s (%s) values (%s)',
+            $table,
+            implode(', ', array_keys($keyValueData)),
+            implode(', ', array_values($keyValueData))
+        );
+
+        try {
+
+            $statement = $this->pdo->prepare($stringFormat);
+            $statement->execute($keyValueData);
+        } catch (Exception $th) {
+
+            die('데이터 베이스 관련 에러 발생');
+        }
+
+        return $stringFormat;
     }
 }
