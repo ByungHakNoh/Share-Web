21e9ae7ee5f5338e1e8e6b4977c013157fff2f31
ByungHakNoh <nbh6579@naver.com>
2020-04-27 16:20:56 +0900

    [add] Home css 파일 링크 추가"

--- views/view_index.php
+++ views/view_index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Share</title>
     <?php require('holder/main/linkCss.php'); ?>
+    <link rel="stylesheet" href="css/mainHomeStyle.css?after">
 </head>
 
 <body>
