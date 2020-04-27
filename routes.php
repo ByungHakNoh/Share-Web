ef719b59c4f071b640a6d68f79c3612840a2bdc5
ByungHakNoh <nbh6579@naver.com>
2020-04-27 16:30:11 +0900

    "[add] 라우트 요소 추가"

--- routes.php
+++ routes.php
@@ -1,12 +1,23 @@
 <?php
 
+// 메인 홈페이지 : 상단 네비게이션 바로 이동 하는 루트
 $router->get('', 'controllers/index.php');
 $router->get('news', 'controllers/news.php');
 $router->get('information', 'controllers/information.php');
 $router->get('board', 'controllers/board.php');
-// 연습용으로 get으로 바꾸기
-$router->post('login', 'controllers/login.php');
-$router->get('register', 'controllers/register.php');
-$router->get('practice', 'controllers/1_practice.php');
+$router->get('broadcast', 'controllers/broadcast.php');
 
-var_dump($_SERVER['REQUEST_METHOD']);
+// 로그인 및 회원가입 루트
+//사용자가 처음 접속했을 때  GET으로 반응
+$router->get('login', 'controllers/login.php');
+$router->get('register', 'controllers/register.php');
+// 회원가입 버튼을 누를 시 POST로 반응
+$router->post('login', 'controllers/login.php');
+$router->post('register', 'controllers/register.php');
+
+$router->get('board-write', 'controllers/write.php');
+$router->post('board-write', 'controllers/write.php');
+
+//연습용 루트
+$router->get('practice', 'controllers/1_practice.php');
+$router->post('practice2', 'controllers/2_practice.php');
