9a53a7a19ce08e6efffb523d33300249bf257292
ByungHakNoh <nbh6579@naver.com>
2020-04-28 13:27:36 +0900

    "[add] postHandler 라우터 추가"

--- routes.php
+++ routes.php
@@ -4,20 +4,18 @@
 $router->get('', 'controllers/index.php');
 $router->get('news', 'controllers/news.php');
 $router->get('information', 'controllers/information.php');
+// 자유계시판 관련 루트
 $router->get('board', 'controllers/board.php');
+$router->get('board-read', 'controllers/board-read.php');
+$router->get('board-write', 'controllers/board-write.php');
 $router->get('broadcast', 'controllers/broadcast.php');
 
 // 로그인 및 회원가입 루트
 //사용자가 처음 접속했을 때  GET으로 반응
 $router->get('login', 'controllers/login.php');
 $router->get('register', 'controllers/register.php');
-// 회원가입 버튼을 누를 시 POST로 반응
-$router->post('login', 'controllers/login.php');
-$router->post('register', 'controllers/register.php');
 
-$router->get('board-write', 'controllers/write.php');
-$router->post('board-write', 'controllers/write.php');
-
-//연습용 루트
-$router->get('practice', 'controllers/1_practice.php');
-$router->post('practice2', 'controllers/2_practice.php');
+// 포스트 메소드로 넘어온 정보를 처리하는 루트
+$router->post('registerHandler', 'controllers/postHandler/registerHandler.php');
+$router->post('board-writeHandler', 'controllers/postHandler/boardWriteHandler.php');
+$router->post('login-startsession', 'controllers/postHandler/sessionHandler/startSession.php');
