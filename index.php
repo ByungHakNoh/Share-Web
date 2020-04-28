634ede163268366b430513706ca052139f786e4a
ByungHakNoh <nbh6579@naver.com>
2020-04-29 00:56:15 +0900

    "[add] controller 파일 불러오는 require 코드 추가"

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 // 웹이 동작하는데 필요한 util 클래스들을 묶어놓은 파일이다.
 require 'core/CoreBundle.php';
+require 'app/controllers/PagesController.php';
+require 'app/controllers/UserController.php';
+
 
 // 라우팅을 위한 작업
 $uri = RequestURI::uriPath();
 
-require Router::load('routes.php')->direct($uri, RequestURI::defineRequestType());
+Router::load('app/routes.php')->direct($uri, RequestURI::defineRequestType());
