4c9934afed85054de582a281b09ad3060245e97d
ByungHakNoh <nbh6579@naver.com>
2020-04-29 00:59:04 +0900

    [add] App 클래스 관련 코드 추가(수정 가능성이 있는 전역 메소드도 추가)"

--- core/CoreBundle.php
+++ core/CoreBundle.php
@@ -1,16 +1,20 @@
 <?php
 
-// 어디서든지 config 나 database에 접근하려면 $app 배열(key-value)을 사용하면 된다.
-$app = [];
-
-$app['config'] = require 'config.php';
-
 require 'core/router/Router.php';
 require 'core/router/RequestURI.php';
 require 'core/database/Connection.php';
 require 'core/database/QueryBuilder.php';
+require 'core/App.php';
 
-$app['database'] = new QueryBuilder(
+// config와 QueryBuilder 객체를 저장하는 App객체를 어느 파일에서도 사용할 수 있게 저장
+App::bind('config', require 'config.php');
+APP::bind('database',  new QueryBuilder(
 
-    Connection::connect_db($app['config']['database'])
-);
+    Connection::connect_db(App::get('config')['database'])
+));
+
+function loadView($view, $data = [])
+{
+    extract($data);
+    return require "app/views/view_{$view}.php";
+}
