ae81c36d1a1ec7ff93d10c8d2871d189ead4a36a
ByungHakNoh <nbh6579@naver.com>
2020-04-30 09:39:27 +0900

    "[add] namespace 추가"

--- core/CoreBundle.php
+++ core/CoreBundle.php
@@ -1,5 +1,10 @@
 <?php
 
+use core\App;
+use core\database\{NameController, QueryBuilder, Connection};
+
+require 'core/mvc/View.php';
+require 'core/mvc/Controller.php';
 require 'core/router/Router.php';
 require 'core/router/RequestURI.php';
 require 'core/database/Connection.php';
@@ -8,13 +13,7 @@ require 'core/App.php';
 
 // config와 QueryBuilder 객체를 저장하는 App객체를 어느 파일에서도 사용할 수 있게 저장
 App::bind('config', require 'config.php');
-APP::bind('database',  new QueryBuilder(
+App::bind('database',  new QueryBuilder(
 
-    Connection::connect_db(App::get('config')['database'])
+    Connection::connectDB(App::get('config')['database'])
 ));
-
-function loadView($view, $data = [])
-{
-    extract($data);
-    return require "app/views/view_{$view}.php";
-}
