56f305df447e7a7da1db3c2c56d9b987b850be8b
ByungHakNoh <nbh6579@naver.com>
2020-05-02 17:11:39 +0900

    "[add] 사용자 정보 변수 추가"

--- app/data/UserInfo.php
+++ app/data/UserInfo.php
@@ -4,17 +4,28 @@ namespace app\data;
 
 class UserInfo
 {
-
-    private $userID;
+    private $user_id;
     private $password;
+    private $nick_name;
+    private $sex;
 
     public function getUserID()
     {
-        return $this->userID;
+        return $this->user_id;
     }
 
     public function getPassword()
     {
         return $this->password;
+    }
+
+    public function getNickName()
+    {
+        return $this->nick_name;
+    }
+
+    public function getSex()
+    {
+        return $this->sex;
     }
 }
