a94a7e4bcb695be424091771dd91ef235d12267c
ByungHakNoh <nbh6579@naver.com>
2020-05-02 17:10:44 +0900

    "[add] Post 메소드 처리하는 코드 추가"

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,7 +4,11 @@ namespace app\controllers;
 
 use core\mvc\Controller;
 
-// request를 받고 -> 데이터베이스와의 동작 -> 결과 반환
+// 사용자의 request를 수신받는다.
+
+// 모델 클래스를 생성하고 필요한 데이터를 DB에서 받아온다.
+
+// 받아온 데이터를 view에 넣고 view 객체를 생성한다.
 class PagesController extends Controller
 {
     public function home()
@@ -36,7 +40,7 @@ class PagesController extends Controller
             $currentPage = 1;
         }
 
-        $model = $this->createModel('Board');
+        $model = $this->createModel('BoardModel');
         $model->fetchPostData($currentPage);
         $viewData = $model->getReturnedData();
 
@@ -48,7 +52,7 @@ class PagesController extends Controller
     {
         $requestPostID = $_GET['id'];
 
-        $model = $this->createModel('BoardReadModel');
+        $model = $this->createModel('BoardModel');
         $model->fetchBoardData($requestPostID);
 
         $viewData = $model->getReturnedData();
@@ -58,8 +62,26 @@ class PagesController extends Controller
 
     public function boardWrite()
     {
-        $view = $this->createView('board-write');
-        return $view->loadView();
+        // 사용자가 게시글을 추가하는 경우
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // 작성자 바꾸기
+            $writer = '노병학';
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $directUrl = 'board';
+
+            $model = $this->createModel('BoardModel');
+            $model->uploadScript($writer, $title, $content);
+
+            header("Location:/{$directUrl}");
+            exit;
+
+            // 글쓰기 페이지에 접속한 경우
+        } else {
+
+            $view = $this->createView('board-write');
+            return $view->loadView();
+        }
     }
 
     public function broadcast()
