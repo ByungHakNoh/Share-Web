7a84368f3cdf31fc577dc5cdba45eb2d25c3dc34
ByungHakNoh <nbh6579@naver.com>
2020-05-04 14:08:02 +0900

    "[add] controller파일 require 코드 추가"

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ use core\router\{Router, RequestURI};
 
 // 웹이 동작하는데 필요한 util 클래스들을 묶어놓은 파일이다.
 require 'core/CoreBundle.php';
-require 'app/controllers/MainController.php';
+require 'app/controllers/PagesController.php';
 require 'app/controllers/UserController.php';
+require 'app/controllers/BoardController.php';
 
-
+session_start();
 // 라우팅을 위한 작업
 $uri = RequestURI::uriPath();
 
