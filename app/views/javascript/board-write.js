50c008787d75919610e163699c30f630e934f47b
ByungHakNoh <nbh6579@naver.com>
2020-05-04 13:56:39 +0900

    "[add] 동영상 업로드하는 기능 추가(수정 필요)"

--- app/views/javascript/board-write.js
+++ app/views/javascript/board-write.js
@@ -1,10 +1,45 @@
-// 자바 스크립트 공부 후 추후 수정해야함
-
-// 빈칸 체크 관련
 const writeForm = document.getElementById("writeForm");
 const title = document.getElementById("title");
 const content = document.getElementById("summernote");
 
+// summernote 실행
+$(document).ready(function () {
+  $("#summernote").summernote({
+    minHeight: 700,
+    maxHeight: 700,
+    focus: true,
+    lang: "KO-KR",
+    callbacks: {
+      onInit: function () {
+        $("#summernote").summernote("code", initContent.substring(1, initContent.length - 1));
+      },
+    },
+  });
+});
+
+// 로컬 동영상 업로드 하는 메소드 (html input[type = 'file']에서 호출함)
+function uploadVideo(inputObject) {
+  const range = $("#summernote").summernote("createRange");
+  const video = document.createElement("video");
+  const videoFile = inputObject.files[0];
+
+  if (videoFile) {
+    const fileReader = new FileReader();
+
+    fileReader.addEventListener("load", function () {
+      video.src = this.result;
+    });
+    fileReader.readAsDataURL(videoFile);
+  }
+
+  video.setAttribute("controls", "controls");
+
+  range.deleteContents();
+  range.insertNode(video);
+  range.collapse(false);
+}
+
+// 빈칸체크하는 리스너 구현
 writeForm.addEventListener("submit", event => {
   if (!title.value || !content.value) {
     event.preventDefault();
@@ -12,13 +47,23 @@ writeForm.addEventListener("submit", event => {
   }
 });
 
-// summernote 관련
-$(document).ready(function () {
-  $("#summernote").summernote({
-    placeholder: "자유 계시판에 올릴 내용을 입력하세요!",
-    minHeight: 700,
-    maxHeight: 700,
-    focus: true,
-    lang: "KO-KR",
-  });
-});
+// inputVideo.addEventListener("change", function () {
+//   const range = $("#summernote").summernote("createRange");
+//   const video = document.createElement("video");
+//   const videoFile = this.files[0];
+
+//   if (videoFile) {
+//     const fileReader = new FileReader();
+
+//     fileReader.addEventListener("load", function () {
+//       video.src = this.result;
+//     });
+//     fileReader.readAsDataURL(videoFile);
+//   }
+
+//   video.setAttribute("controls", "controls");
+
+//   range.deleteContents();
+//   range.insertNode(video);
+//   range.collapse(false);
+// });
