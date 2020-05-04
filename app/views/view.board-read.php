88d4a00f45ede8f2098e61e5d93851c36b13801b
ByungHakNoh <nbh6579@naver.com>
2020-05-04 13:59:42 +0900

    "[add] 수정, 삭제 버튼 추가"

--- app/views/view.board-read.php
+++ app/views/view.board-read.php
@@ -12,16 +12,42 @@
 <body>
     <?php require('holder/main/bodyHeadPage.php'); ?>
 
-    <div class="readContainer">
-        <div id="information">
-            <h1>제목 : <?= $postElements[0]->getTitle() ?></h1>
-            <h3>글쓴이 : <?= $postElements[0]->getWriter() ?></h3>
-            <h5>글쓴 날짜 : <?= $postElements[0]->getDate() ?></h5>
-            <h5>조회수 : <?= $postElements[0]->getHit() + 1 ?></h5>
-        </div>
+    <div class="container">
+        <div class="readContainer">
+            <div id="information">
+                <h1>제목 : <?= $postElements[0]->getTitle() ?></h1>
+                <h3>글쓴이 : <?= $postElements[0]->getWriter() ?></h3>
+                <h5>글쓴 날짜 : <?= $postElements[0]->getDate() ?></h5>
+                <h5>조회수 : <?= $postElements[0]->getHit() + 1 ?></h5>
+            </div>
 
-        <div id="content">
-            <?= $postElements[0]->getContent() ?>
+            <div id="content">
+                <?= $postElements[0]->getContent() ?>
+            </div>
+
+            <!-- 동일 닉네임이면 수정 삭제 버튼 생성 -->
+            <?php if (isset($_SESSION['nickName'])) : ?>
+            <?php if ($_SESSION['nickName'] === $postElements[0]->getWriter()) : ?>
+            <div class="btnContainer">
+                <form method="POST" action="/board-delete">
+                    <input type="hidden" name="id" value=<?= $postElements[0]->getID() ?>>
+                    <input id="deleteSubmit" type="submit" value="삭제">
+                </form>
+                <form method="POST" action="/board-write">
+                    <input type="hidden" name="id" value=<?= $postElements[0]->getID() ?>>
+                    <input id="modifySubmit" type="submit" value="수정">
+                </form>
+            </div>
+
+            <?php endif ?>
+            <?php endif ?>
+
+            <div class="commentContainer">
+                <form method="POST" action="/board-writeHandler">
+                    <textArea></textArea>
+                    <input type="submit" value="등록">
+                </form>
+            </div>
         </div>
     </div>
 </body>
