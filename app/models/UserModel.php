d724b870e63eabaedd8d8fbc1b31e6ca7cd943ad
ByungHakNoh <nbh6579@naver.com>
2020-05-04 13:55:10 +0900

    "[add] 로그인 기능 추가"

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -21,19 +21,45 @@ class UserModel extends Model
         ]);
     }
 
-    // 로그인 
+    // 회원가입 시 중복 체크를 하기 위해 사용자 정보를 db로부터 가져오는 메소드
     public function fetchAllUser()
     {
         $userID = 'forsythia01@gmail.com';
 
         $tableName = 'user';
-        // $keyValueData = ['user_id' => $userID];
-        $dataClass = 'app\data\UserInfo';
-
 
         $statement = App::get('database')->selectTable($tableName);
         $userList = App::get('database')->fetchAllByArray($statement);
 
         $this->returnedData = ['userList' => $userList];
+    }
+
+    public function checkLoginInput($userID, $password)
+    {
+        $tableName = 'user';
+        $dataClass = 'app\data\UserInfo';
+
+        $userInfo = App::get('database')->fetchLoginData($tableName, $userID, $password, $dataClass);
+
+        if ($userInfo == null) {
+            $message = '아이디 혹은 비밀번호를 확인해주세요';
+            $this->returnedData = [
+                'userID' => $userID,
+                'loginError' => $message
+            ];
+        }
+        return $userInfo;
+    }
+
+    public function startSession($userInfo)
+    {
+        $userID =  $userInfo[0]->getUserID();
+        $nickName = $userInfo[0]->getNickName();
+        $sex = $userInfo[0]->getSex();
+
+        // 세션에 사용자 정보를 등록한다
+        $_SESSION['userID'] = $userID;
+        $_SESSION['nickName'] = $nickName;
+        $_SESSION['sex'] = $sex;
     }
 }
