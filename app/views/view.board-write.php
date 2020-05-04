bcdf8c57a10547ac5a075e7931b2cdbafc8fa295
ByungHakNoh <nbh6579@naver.com>
2020-05-04 14:01:19 +0900

    "[add] 자유게시글 수정 시  초기 데이터 설정"

--- app/views/view.board-write.php
+++ app/views/view.board-write.php
@@ -18,22 +18,26 @@
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.16/dist/summernote.min.js"></script>
 
-    <!-- 라이브러리 폴더에 위치해 있음 다시 확인할 필요가 있다. -->
-    <script src="library/summernote-ko-KR.js"></script>
     <!-- 자바 스크립트 코드 불러오기 -->
+    <script>
+    let initContent = '<?= json_encode(isset($postElements) ? $postElements[0]->getContent() : "") ?>'
+    </script>
     <script defer src="app/views/javascript/board-write.js"></script>
 </head>
 
 <body>
     <?php require('holder/main/bodyHeadPage.php'); ?>
-
     <div class="writeContainer">
-        <form id="writeForm" method="post" action="/board-writeHandler">
-            <!-- <form id="writeForm"> -->
-            <h4>제목 : <input id="title" type="text" name="title" /></h4>
+        <form id="writeForm" method="POST" action="/board-writeHandler">
+            <h4>제목 : <input id="title" type="text" name="title"
+                    value="<?= isset($postElements) ? $postElements[0]->getTitle() : null ?>"></h4>
             <textarea id="summernote" name="content"></textarea>
+            <input type="hidden" name="id" value=<?= isset($requestID) ? $requestID : null ?>>
             <input type="submit" value="확인" />
         </form>
+
+        <!-- <button id="uploadVideoBtn">동영상 업로드</button> -->
+        <input id="videoInput" oninput="uploadVideo(this)" type="file">
     </div>
 
 </body>
