7d7007e1d71ffa483c4a28db09526d3d8417de10
ByungHakNoh <nbh6579@naver.com>
2020-05-04 14:02:13 +0900

    "[add] 로그인 시 경고문 레이아웃 추가"

--- app/views/view.login.php
+++ app/views/view.login.php
@@ -16,13 +16,17 @@
     <div class=container>
         <img src="src/image/log_in.png" class="icon">
         <h1>로그인 하기</h1>
+        <small><?= isset($loginError) ? $loginError : "" ?></small>
 
-        <form method="POST" action="/login-startsession">
+        <form method="POST" action="/login-startSession">
             <p>사용자 아이디</p>
-            <input type="text" name="userID" placeholder="이메일 입력">
+            <input id="userID" type="text" name="userID" placeholder="이메일 입력"
+                value=<?= isset($userID) ? $userID : ''; ?>>
+            <small id="userIDText"></small>
 
             <p>비밀번호</p>
-            <input type="password" name="password" placeholder="비밀번호 입력">
+            <input id="password" type="password" name="password" placeholder="비밀번호 입력">
+            <small id="passwordText"></small>
 
             <input id="submitBtn" type="submit" value="로그인">
             <a href="#">아이디/비밀번호 찾기</a>
