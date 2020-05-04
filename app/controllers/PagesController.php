19bf0cbfded91870d4d603e13aefada4f2657aaf
ByungHakNoh <nbh6579@naver.com>
2020-05-04 13:52:26 +0900

    "[add] 웹의 전반적인 페이지를 담당하는 컨트롤러 추가"

---
+++ app/controllers/PagesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers;
+
+use core\mvc\Controller;
+
+// 사용자의 request를 수신받는다.
+
+// 모델 클래스를 생성하고 필요한 데이터를 DB에서 받아온다.
+
+// 받아온 데이터를 view에 넣고 view 객체를 생성한다.
+class PagesController extends Controller
+{
+    public function home()
+    {
+        $view = $this->createView('home');
+        return $view->loadView();
+    }
+
+    public function information()
+    {
+        $view = $this->createView('information');
+        return $view->loadView();
+    }
+
+    public function news()
+    {
+        $view = $this->createView('news');
+        return $view->loadView();
+    }
+
+    public function broadcast()
+    {
+        $view = $this->createView('broadcast');
+        return $view->loadView();
+    }
+}
