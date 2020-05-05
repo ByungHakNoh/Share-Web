7dc4cf29fa4754157eca9b10e5058c59aad00a57
ByungHakNoh <nbh6579@naver.com>
2020-05-05 11:52:51 +0900

    "[add] 쿠키 기능 추가"

--- app/controllers/BoardController.php
+++ app/controllers/BoardController.php
@@ -30,8 +30,10 @@ class BoardController extends Controller
 
         $model = $this->createModel('BoardModel');
         $model->fetchBoardData($requestID);
+        $model->fetchCommentData($requestID);
 
         $viewData = $model->getReturnedData();
+        $viewData['model'] = $model;
         $view = $this->createView('board-read', $viewData);
         return $view->loadView();
     }
@@ -88,12 +90,93 @@ class BoardController extends Controller
     public function boardDeletePost()
     {
         $requestID = $_POST['id'];
-        die($requestID);
         $directUrl = 'board';
 
         $model = $this->createModel('BoardModel');
         $model->deletePost($requestID);
         header("Location:/{$directUrl}");
+        exit;
+    }
+
+    // 게시판 댓글 추가 수정을 관리
+    public function boardComment()
+    {
+        $postID = $_POST['id'];
+        $directUrl = 'board-read';
+        $model = $this->createModel('BoardModel');
+
+        //댓글을 추가할 경우
+        if (!isset($_POST['modifiedCommentID'])) {
+            $writer = $_POST['nickName'];
+            $comment = $_POST['comment'];
+
+            $model->addComment($postID, $writer, $comment);
+
+            header("Location:/{$directUrl}?id={$postID}");
+            exit;
+
+            // 댓글을 수정할 경우
+        } else {
+
+            $modifiedCommentID = $_POST['modifiedCommentID'];
+            $modifiedComment = $_POST['modifiedComment'];
+
+            $model->modifyComment($modifiedCommentID, $modifiedComment);
+
+            header("Location:/{$directUrl}?id={$postID}");
+            exit;
+        }
+    }
+
+    public function boardDeleteComment()
+    {
+        $deleteCommentID = $_POST['deleteCommentID'];
+        $postID = $_POST['id'];
+        $directUrl = 'board-read';
+
+        $model = $this->createModel('BoardModel');
+        $model->deleteComment($deleteCommentID);
+        header("Location:/{$directUrl}?id={$postID}");
+        exit;
+    }
+
+    public function boardReply()
+    {
+        $postID = $_POST['id'];
+        $directUrl = 'board-read';
+        $model = $this->createModel('BoardModel');
+
+        if (!isset($_POST['modifiedReplyID'])) {
+
+            $commentID = $_POST['commentID'];
+            $writer = $_POST['writer'];
+            $reply = $_POST['reply'];
+
+            $model->addReply($commentID, $writer, $reply);
+
+            header("Location:/{$directUrl}?id={$postID}");
+            exit;
+        } else {
+
+            $modifiedReplyID = $_POST['modifiedReplyID'];
+            $modifiedReply = $_POST['modifiedReply'];
+
+            $model->modifyReply($modifiedReplyID, $modifiedReply);
+
+            header("Location:/{$directUrl}?id={$postID}");
+            exit;
+        }
+    }
+
+    public function boardDeleteReply()
+    {
+        $postID = $_POST['id'];
+        $deleteReplyID = $_POST['deleteReplyID'];
+        $directUrl = 'board-read';
+
+        $model = $this->createModel('BoardModel');
+        $model->deleteReply($deleteReplyID);
+        header("Location:/{$directUrl}?id={$postID}");
         exit;
     }
 }
