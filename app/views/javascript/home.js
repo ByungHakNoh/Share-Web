17994a1d066dbade523e2d43b78242e8c4feb473
ByungHakNoh <nbh6579@naver.com>
2020-05-06 17:59:24 +0900

    [add] 광고 닫기 버튼 리스너 추가

---
+++ app/views/javascript/home.js
@@ -0,0 +1,6 @@
+const closeBtn = document.getElementById("closeBtn");
+const cookieContainer = document.getElementById("cookieContainer");
+
+closeBtn.addEventListener("click", function () {
+  cookieContainer.hidden = true;
+});
