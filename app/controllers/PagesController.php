39705d772cfa5a2785bcc6cfd06aba180330a482
EC2 Default User <ec2-user@ip-172-31-38-16.ap-northeast-2.compute.internal>
2020-05-09 08:46:57 +0000

    [add] 오늘의 방문자 수 중복을 제외하기 위해 쿠키 사용

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -3,7 +3,6 @@
 namespace app\controllers;
 
 use core\mvc\Controller;
-use core\router\RequestURI;
 
 // 사용자의 request를 수신받는다.
 
@@ -14,6 +13,16 @@ class PagesController extends Controller
 {
     public function home()
     {
+        // 처음 방문한 경우 하루 동안 쿠키 저장
+        if (!isset($_COOKIE['visited'])) {
+            setcookie('visited', true, time() + 86400);
+
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+            $model = $this->createModel('UserModel');
+            $model->uploadAdminData($ip, $userAgent);
+        }
         $view = $this->createView('home');
         return $view->loadView();
     }
@@ -39,10 +48,12 @@ class PagesController extends Controller
     public function cookieHandler()
     {
 
+        // 홈페이지의 '오늘 광고 그만보기'를 눌렀을 경우
         if (isset($_POST['notToday'])) {
             setcookie('notToday', true, time() + 86400);
+
+            header("Location:/");
+            exit;
         }
-        header("Location:/");
-        exit;
     }
 }
