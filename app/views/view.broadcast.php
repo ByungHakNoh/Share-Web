3c301c1ccb36899701b75dcc86e8c867d6b4544d
EC2 Default User <ec2-user@ip-172-31-38-16.ap-northeast-2.compute.internal>
2020-05-11 05:27:34 +0000

    [add] 방송 영상 송출하는 레이아웃 추가

--- app/views/view.broadcast.php
+++ app/views/view.broadcast.php
@@ -6,12 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Share - broadcast</title>
     <?php require('holder/main/linkCss.php'); ?>
+    <link rel="stylesheet" href="public/css/broadcastStyle.css?after">
+    <link href="https://vjs.zencdn.net/7.1/video-js.min.css" rel="stylesheet">
+    <script src="https://vjs.zencdn.net/7.1/video.min.js"></script>
 </head>
 
 <body>
 
     <?php require('holder/main/bodyHeadPage.php'); ?>
+    <div class="broadcastContainer">
+        <h1>... 의 방송입니다</h1>
+        <video id="player" class="video-js vjs-default-skin" controls>
+            <source src="https://share-fashion.ga/src/video/hls/test.m3u8" type="application/x-mpegURL" />
+        </video>
+    </div>
+    <script>
+        let options, video;
 
+        options = {
+            autoplay: true,
+            muted: true
+        };
+
+        video = videojs('player', options);
+    </script>
 </body>
 
 </html>
