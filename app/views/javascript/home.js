ae761a16f82a0ce62ae3044a5e216aa10ce78906
EC2 Default User <ec2-user@ip-172-31-38-16.ap-northeast-2.compute.internal>
2020-05-11 05:24:02 +0000

    [add] 자바스크립트  텝 버튼관련 코드 추가

--- app/views/javascript/home.js
+++ app/views/javascript/home.js
@@ -1,6 +1,25 @@
 const closeBtn = document.getElementById("closeBtn");
 const cookieContainer = document.getElementById("cookieContainer");
 
+const recentPostBtn = document.getElementById("recentPostBtn");
+const viewsPostBtn = document.getElementById("viewsPostBtn");
+const recentPostContainer = document.getElementById("recentPostContainer");
+const viewsPostContainer = document.getElementById("viewsPostContainer");
+
 closeBtn.addEventListener("click", function () {
   cookieContainer.hidden = true;
+});
+
+recentPostBtn.addEventListener("click", function () {
+  recentPostContainer.hidden = false;
+  viewsPostContainer.hidden = true;
+  this.className = "active";
+  viewsPostBtn.className = "";
+});
+
+viewsPostBtn.addEventListener("click", function () {
+  recentPostContainer.hidden = true;
+  viewsPostContainer.hidden = false;
+  this.className = "active";
+  recentPostBtn.className = "";
 });
