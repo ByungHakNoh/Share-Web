b38f916bf1c040c15ba4b892cd503bcd163aee3a
EC2 Default User <ec2-user@ip-172-31-38-16.ap-northeast-2.compute.internal>
2020-05-11 05:30:05 +0000

    [add] 월별 방문자, 브라우저, 성비 차트 추가

--- app/views/javascript/admin.js
+++ app/views/javascript/admin.js
@@ -1,13 +1,11 @@
 // 관리자 페이지 차트 tap 관리
-const tabBtns = document.querySelectorAll(".tapBtnContainer");
+const tabBtns = document.getElementsByClassName("tapBtnContainer");
 const tabPanels = document.querySelectorAll(".adminTabPannel");
 
-console.log(visitorCountries);
-console.log(JSON.parse(monthlyVisitor));
+console.log(tabBtns[0]);
 
 // tab 클릭 시 이벤트
 function panelController(panelIndex) {
-  console.log(panelIndex);
   // 전체 보기
   if (panelIndex == 0) {
     tabPanels.forEach(function (node) {
@@ -27,16 +25,53 @@ function panelController(panelIndex) {
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(countryChart);
 google.charts.setOnLoadCallback(monthlyVisitorChart);
+google.charts.setOnLoadCallback(browserChart);
+google.charts.setOnLoadCallback(memberSexRatioChart);
 
 // 차트 보여주는 메소드들 모음
 function countryChart() {
   let data = google.visualization.arrayToDataTable(JSON.parse(visitorCountries));
 
   let options = {
-    title: "나라별 접속자 수",
+    title: "나라별 접속자 비율",
   };
 
   let chart = new google.visualization.PieChart(document.getElementById("visitorByCountry"));
+
+  chart.draw(data, options);
+}
+
+function monthlyVisitorChart() {
+  var data = google.visualization.arrayToDataTable(JSON.parse(monthlyVisitor));
+
+  var options = {
+    title: "월별 접속자 수",
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById("monthlyVisitor"));
+  chart.draw(data, options);
+}
+
+function browserChart() {
+  let data = google.visualization.arrayToDataTable(JSON.parse(visitorBrowser));
+
+  let options = {
+    title: "브라우저 별 접속자 비율",
+  };
+
+  let chart = new google.visualization.PieChart(document.getElementById("browser"));
+
+  chart.draw(data, options);
+}
+
+function memberSexRatioChart() {
+  let data = google.visualization.arrayToDataTable(JSON.parse(memberSexRatio));
+
+  let options = {
+    title: "회원 성비",
+  };
+
+  let chart = new google.visualization.PieChart(document.getElementById("sexRatio"));
 
   chart.draw(data, options);
 }
